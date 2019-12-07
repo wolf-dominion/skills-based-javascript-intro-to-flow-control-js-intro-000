2f8925d6f712a6a2ac79e2e204fd9f4599fa9288
Hannah Drury <hannah_drury@yahoo.com>
2019-12-07 05:13:22 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -43,6 +43,18 @@ switch(x){
   case 15: 
     console.log ("You are a teenager");
     break;
+  case 16: 
+    console.log ("You are a teenager");
+    break;
+  case 17: 
+    console.log ("You are a teenager");
+    break;
+  case 18: 
+    console.log ("You are a teenager");
+    break;
+  case 19: 
+    console.log ("You are a teenager");
+    break;
     
   default: 
     console.log ("You have an age");
