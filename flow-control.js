fe4e271b4008873a21cb6fc1538ff0ac4728d708
Hannah Drury <hannah_drury@yahoo.com>
2019-12-07 05:14:16 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -32,31 +32,31 @@ return (x >= 13 && x <= 19) ? 'You are a teenager' : 'You are not a teenager';
 function switchAge(x) {
 switch(x){
   case 12: 
-    console.log ("You are a teenager");
+    console.log ("You are a teenager")
     break;
   case 13: 
-    console.log ("You are a teenager");
+    console.log ("You are a teenager")
     break;
   case 14: 
-    console.log ("You are a teenager");
+    console.log ("You are a teenager")
     break;
   case 15: 
-    console.log ("You are a teenager");
+    console.log ("You are a teenager")
     break;
   case 16: 
-    console.log ("You are a teenager");
+    console.log ("You are a teenager")
     break;
   case 17: 
-    console.log ("You are a teenager");
+    console.log ("You are a teenager")
     break;
   case 18: 
-    console.log ("You are a teenager");
+    console.log ("You are a teenager")
     break;
   case 19: 
-    console.log ("You are a teenager");
+    console.log ("You are a teenager")
     break;
   default: 
-    console.log ("You have an age");
+    console.log ("You have an age")
 }
 }
 
